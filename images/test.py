cc0d7495b641787769f0ffeb1d770c480f048d7e
koukoufilippasn <nick.koukoufilippas@physics.ox.ac.uk>
2019-10-24 11:51:16 +0100

    test plots

--- images/test.py
+++ images/test.py
@@ -1,18 +1,26 @@
 """
-temp file
+plot measured data from NaMaster via pipeline.py versus Planck data
 """
 
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+# change the next 2 lines only
+XX = "MV"
+SZdeproj = False
 
-my_data = np.load("output_lensing/cls_lens_lens.npz")
+A = ("szdeproj_" if SZdeproj else "") + XX
+
+my_data = np.load("../output_default/cls_lens_%s_lens_%s.npz" % (A, A))
 
 my_ells = my_data["ls"]
 my_cls = my_data["cls"]
 
-planck_data = np.loadtxt("data/maps/nlkk.dat")
+try:
+    planck_data = np.loadtxt("../data/maps/COM_Lensing_4096_R3.00_%s_nlkk.dat" % A)
+except OSError:
+    planck_data = np.loadtxt("../data/maps/COM_Lensing_Szdeproj_4096_R3.00_%s_nlkk.dat" % XX)
 
 planck_ells = planck_data[:, 0]
 planck_cls = planck_data[:, 2]
@@ -30,4 +38,4 @@ plt.loglog()
 plt.legend(loc="best")
 plt.xlabel(r"$\ell$")
 plt.ylabel(r"$C_{\ell}$")
-plt.savefig("lensing_MV_no_derpoj_Planck_vs_mine.pdf")
+plt.savefig("lensing_%s_Planck_vs_mine.pdf" % A)
