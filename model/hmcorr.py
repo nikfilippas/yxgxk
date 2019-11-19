d6828b26ef0da6ebd701a75c003d214703902061
koukoufilippasn <nick.koukoufilippas@physics.ox.ac.uk>
2019-11-19 13:41:36 +0000

    hmcorr now in one script

--- model/hmcorr.py
+++ model/hmcorr.py
@@ -4,6 +4,7 @@ Approximates the Halo Model correction as a Gaussian.
 
 import numpy as np
 import pyccl as ccl
+from scipy.interpolate import interp1d
 from scipy.interpolate import interp2d
 from scipy.optimize import curve_fit
 
@@ -68,18 +69,18 @@ gauss = lambda k, A, k0, s: 1 + A*np.exp(-0.5*(np.log10(k/k0)/s)**2)
 
 
 k = np.geomspace(0.1, 5, 128)
-z = np.linspace(0, 1, 16)
-a = 1/(1+z)
+hm_z = np.linspace(0, 1, 16)
+hm_a = 1/(1+hm_z)
 
 
 
-POPT_T08 = [[] for i in range(z.size)]
-PCOV_T08 = [[] for i in range(z.size)]
+POPT_T08 = [[] for i in range(hm_z.size)]
+PCOV_T08 = [[] for i in range(hm_z.size)]
 
-POPT_T10 = [[] for i in range(z.size)]
-PCOV_T10 = [[] for i in range(z.size)]
+POPT_T10 = [[] for i in range(hm_z.size)]
+PCOV_T10 = [[] for i in range(hm_z.size)]
 
-for i, aa in enumerate(a):
+for i, aa in enumerate(hm_a):
     # Tinker 2008
     popt, pcov = curve_fit(gauss, k, hfT08(k, aa))
     POPT_T08[i] = popt
@@ -95,7 +96,21 @@ BF_T08 = np.vstack(POPT_T08)[:, 1:]
 BF_T10 = np.vstack(POPT_T08)[:, 1:]
 
 
+def HM_correction(mf):
+    if mf == "tinker":
+        k0_func = interp1d(hm_a, BF_T08[:, 0])
+        s_func = interp1d(hm_a, BF_T08[:, 1])
+    elif mf == "tinker10":
+        k0_func = interp1d(hm_a, BF_T10[:, 0])
+        s_func = interp1d(hm_a, BF_T10[:, 1])
+    else:
+        raise ValueError("Mass function not recognised in HM correction.")
+    return k0_func, s_func
 
+
+
+
+"""
 ''' meta-calculations '''
 bft08 = np.vstack(POPT_T08)
 cvt08 = np.vstack(PCOV_T08)
@@ -105,7 +120,8 @@ cvt10 = np.vstack(PCOV_T10)
 
 # p0 for free parameter ``a``
 a_bf = (bft08[:, 0].mean() + bft10[:, 0].mean())/2
-print("A_bf = %.16f" % a_bf) # 0.3609866406234923
+print("A_bf = %.16f" % a_bf)
+# A_bf = 0.3614146096356469
 
 # theoretical error for (k0, s)
 errt08 = cvt08/bft08
@@ -115,3 +131,6 @@ maxerr = 100*errt08[:, 1:].max(), 100*errt10[:, 1:].max()
 merr = 100*errt08[:, 1:].mean(), 100*errt10[:, 1:].mean()
 print("max %% error (T08, T10): %.2f, %.2f" % maxerr)
 print("mean %% error (T08, T10): %.2f, %.2f" % merr)
+# max % error (T08, T10): 2.36, 1.85
+# mean % error (T08, T10): 1.83, 1.01
+"""
