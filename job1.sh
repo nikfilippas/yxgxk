1d5ce9e0c6762833277f265458286c7fe8ceaf12
nikfilippas@gmail.com <nikfilippas@gmail.com>
2020-03-13 00:51:21 +0000

    split jobs for glamdring

--- job1.sh
+++ job1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 alias python='/mnt/zfsusers/nikfilippas/anaconda3/bin/python'
-python pipeline.py params_lensing.yml
+python pipeline.py params1.yml
 #python run_pipeline.py params_lensing.yml --full
--- job2.sh
+++ job2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 alias python='/mnt/zfsusers/nikfilippas/anaconda3/bin/python'
-python pipeline.py params_lensing.yml
+python pipeline.py params2.yml
 #python run_pipeline.py params_lensing.yml --full
