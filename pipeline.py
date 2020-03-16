e1a852af38d72ece65cecdbbe203af34b26603a2
Nikolaos Koukoufilippas <nikfilippas@gmail.com>
2020-03-16 22:47:11 +0000

    added JK number option

--- pipeline.py
+++ pipeline.py
@@ -10,18 +10,20 @@ parser.add_argument("--jk", action="store_false")
 args = parser.parse_args()
 fname_params = args.fname_params
 
+
 p = ParamRun(fname_params)
 # Create output directory if needed
 os.system('mkdir -p ' + p.get_outdir())
-
-print("Computing power spectra...", end="")
 fields = pu.read_fields(p)
-xcorr = pu.get_xcorr(p, fields); print("OK")
-print("Generating theory power spectra")
-mcorr = pu.model_xcorr(p, fields, xcorr)
-print("Computing covariances...")
-pu.get_cov(p, fields, xcorr, mcorr,
-           data=True, model=True, trispectrum=True, jackknife=False)
+
+if not args.jk:
+    print("Computing power spectra...", end="")
+    xcorr = pu.get_xcorr(p, fields); print("OK")
+    print("Generating theory power spectra")
+    mcorr = pu.model_xcorr(p, fields, xcorr)
+    print("Computing covariances...")
+    pu.get_cov(p, fields, xcorr, mcorr,
+               data=True, model=True, trispectrum=True, jackknife=False)
 
 # Jackknives
 if args.jk:
