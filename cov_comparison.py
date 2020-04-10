6c93600cd4eca5bdde67ad437bd4e5f038f8395a
nikfilippas@gmail.com <nikfilippas@gmail.com>
2020-04-10 23:05:36 +0300

    new layout of covariance comparison

--- cov_comparison.py
+++ cov_comparison.py
@@ -1,43 +1,77 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-zbins = ["2mpz"] + ["wisc%d" % i for i in range(1, 6)]
-zbins = None
 
-
-def diff_cov(string):
+def diff_cov(string, diff=True, cov_type=None):
     I = 3 if "y_milca" in string else 2
     new_s = ("_".join(string.split("_")[:I])).split("_")
     new_s.append(new_s[0]); del(new_s[0])
     new_s = "_".join(new_s)
+
     leff = np.load("../yxg/output_default/cls_%s.npz" % new_s)["ls"]
 
-
-    cov_old = np.load("../yxg/output_default/cov_data_%s.npz" % string)
+    fname = "/output_default/cov_%s_%s.npz" % (cov_type, string)
+    if cov_type == "1h4pt":
+        fname = "dcov".join(fname.split("cov"))
+    cov_old = np.load("../yxg"+fname)
     diag_old = np.diag(cov_old["cov"])
-    cov_new = np.load("../yxgxk/output_default/cov_data_%s.npz" % string)
+    cov_new = np.load("../yxgxk"+fname)
     diag_new = np.diag(cov_new["cov"])[:diag_old.size]
-    dd = 1 - diag_new/diag_old
-    print(string[:6])
-    print(diag_old)
-    print(diag_new)
+
+    if diff:
+        dd = 1 - diag_new/diag_old
+    else:
+        dd = (diag_old, diag_new)
+
+    # print(string[:6])
+    # print(diag_old)
+    # print(diag_new)
+
     return dd, leff
 
 
 
-fig, ax = plt.subplots(2, 1, sharex=True)
+fig, ax = plt.subplots(2, 6, sharex=True, sharey="row", figsize=(15,7))
+zbins = ["2mpz"] + ["wisc%d" % i for i in range(1, 6)]
+colors = ["red", "orange", "gold", "green", "blue", "violet"]
+alphas = [0.3, 1.0]
+for i, (col, zbin) in enumerate(zip(colors, zbins)):
 
-
-for zbin in zbins:
     string = "_".join([zbin]*4)
-    dg, leffg = diff_cov(string)
+    dgm, leff = diff_cov(string, diff=False, cov_type="model")
+    dgd, _ = diff_cov(string, diff=False, cov_type="data")
+    dgt, _ = diff_cov(string, diff=False, cov_type="1h4pt")
+
     string = "_".join([zbin+"_y_milca"]*2)
-    dy, leffy = diff_cov(string)
+    dym, _ = diff_cov(string, diff=False, cov_type="model")
+    dyd, _ = diff_cov(string, diff=False, cov_type="data")
+    dyt, _ = diff_cov(string, diff=False, cov_type="1h4pt")
 
-    ax[0].semilogx(leffg, dg, label="%s" % zbin)
-    ax[1].semilogx(leffy, dy, label="%s" % zbin)
 
-ax[0].set_ylabel("delta_cov_gg")
-ax[1].set_ylabel("delta_cov_gy")
-plt.legend(loc="best", ncol=2)
-plt.savefig("comparison_cov_overplot_data.pdf")
+    # [ax[0, i].loglog(leff, dgm[j], ls="-", c=col, alpha=alphas[j % 2],
+    #                   label="%s" % zbin) for j in range(2)]
+
+    # [ax[1, i].loglog(leff, dym[j], ls="-", c=col, alpha=alphas[j % 2],
+    #                   label="%s" % zbin) for j in range(2)]
+
+    [ax[0, i].loglog(leff, dgd[j], ls="--", c=col, alpha=alphas[j % 2],
+                      label="%s" % zbin) for j in range(2)]
+
+    [ax[1, i].loglog(leff, dyd[j], ls="--", c=col, alpha=alphas[j % 2],
+                      label="%s" % zbin) for j in range(2)]
+
+    [ax[0, i].loglog(leff, dgt[j], ls=":", c=col, alpha=alphas[j % 2],
+                      label="%s" % zbin) for j in range(2)]
+
+    [ax[1, i].loglog(leff, dyt[j], ls=":", c=col, alpha=alphas[j % 2],
+                      label="%s" % zbin) for j in range(2)]
+
+
+
+ax[0, 0].set_ylabel(r"$\mathrm{Cov} (g,g)$", fontsize=12)
+ax[1, 0].set_ylabel(r"$\mathrm{Cov} (g,y)$", fontsize=12)
+[ax[1, j].set_xlabel(r"$\ell$", fontsize=12) for j in range(6)]
+[ax[0, j].text(0.40, 1.02, zbins[j], fontsize=14, transform=ax[0,j].transAxes) for j in range(6)]
+# plt.legend(loc="best", ncol=2)
+fig.tight_layout(w_pad=0., h_pad=0.)
+# plt.savefig("comparison_cov_overplot_data.pdf")
