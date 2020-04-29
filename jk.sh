996764dd0a0ae5b97498acff281ecf8f5dd95bbf
nikfilippas@gmail.com <nikfilippas@gmail.com>
2020-04-29 10:13:19 +0300

    3rd JK run

--- jk.sh
+++ jk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for jk in {293..455}
+for jk in {297..388}
 do
     addqueue -s -c "jk ${jk}" -n 1x8 -q "cmb" -m 4 '/mnt/zfsusers/nikfilippas/anaconda3/bin/python' pipeline.py params_lensing.yml --jk-id ${jk}
 done
