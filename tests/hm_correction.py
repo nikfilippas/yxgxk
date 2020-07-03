5cec748420107aec1bcaf44109ff28095529638e
nikfilippas@gmail.com <nikfilippas@gmail.com>
2020-07-03 15:57:54 +0300

    fixed relative imports

--- tests/hm_correction.py
+++ tests/hm_correction.py
@@ -3,10 +3,12 @@ Unit test for Gaussian approximation of the halo model correction.
 The approximation agrees with HaloFit within 4%.
 """
 
+import os, sys
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import numpy as np
 import pyccl as ccl
-from ..model.hmcorr import HaloModCorrection
-from ..model.hmcorr import HM_halofit
+from model.hmcorr import HaloModCorrection
+from model.hmcorr import HM_halofit
 
 
 k_arr = np.geomspace(0.1, 2, 1000)   # wave number
@@ -23,7 +25,6 @@ HM = HaloModCorrection(cosmo)
 a_HMcorr = HM.af(a_arr)
 for i, a in enumerate(a_arr):
     hm_old = HM_halofit(cosmo).rk_interp(k_arr, a)
-    kwargs = {"mass_function": "tinker",
-              "a_HMcorr": a_HMcorr[i]}
+    kwargs = {"a_HMcorr": a_HMcorr[i]}
     hm_new = HM.hm_correction(k_arr, a, **kwargs)
     assert (np.fabs(1-hm_new/hm_old) < 0.04).all()
