f5be26d3e3379c4aac1b82dd4265ee5c29d76344
nikfilippas@gmail.com <nikfilippas@gmail.com>
2020-07-04 15:44:21 +0300

    finalised HM correction approximation

--- model/hmcorr.py
+++ model/hmcorr.py
@@ -14,27 +14,40 @@ class HM_halofit(object):
     model in the 1h - 2h transition regime.
 
     Args:
-        cosmo (:obj:`ccl.Cosmology`): cosmology.
+        cosmo (`pyccl.Cosmology`): cosmology.
         k_range (list): range of k to use (in Mpc^-1).
         nlk (int): number of samples in log(k) to use.
         z_range (list): range of redshifts to use.
         nz (int): number of samples in redshift to use.
+        Delta (int): mass definition overdensity parameter.
+        rho_type (str): 'critical' or 'matter'
+        **kwargs (dict): mass function and halo bias models
 
+    .. note: original HaloFit used `rho_200m`
     .. note: ``interp2d`` flips secondary axis
     """
     def __init__(self, cosmo,
                   k_range=[1E-1, 5], nlk=20,
-                  z_range=[0., 1.], nz=16):
-        karr = np.geomspace(k_range[0], k_range[1], nlk)
-        zarr = np.linspace(z_range[0], z_range[1], nz)
+                  z_range=[0., 1.], nz=16,
+                  Delta=200, rho_type='matter',
+                  **kwargs):
+        k_arr = np.geomspace(k_range[0], k_range[1], nlk)
+        a_arr = 1/(1+np.linspace(z_range[0], z_range[1], nz))
 
-        pk_hm = np.array([ccl.halomodel_matter_power(cosmo, karr, a)
-                          for a in 1. / (1 + zarr)])
-        pk_hf = np.array([ccl.nonlin_matter_power(cosmo, karr, a)
-                          for a in 1. / (1 + zarr)])
+        hmd = ccl.halos.MassDef(Delta, rho_type)
+        nM = kwargs["mass_function"](cosmo, mass_def=hmd)
+        bM = kwargs["halo_bias"](cosmo, mass_def=hmd)
+        hmc = ccl.halos.HMCalculator(cosmo, nM, bM, hmd)
+        cM = ccl.halos.ConcentrationDuffy08(hmd)
+        NFW = ccl.halos.profiles.HaloProfileNFW(cM)
+        pk_hm = ccl.halos.halomod_power_spectrum(cosmo, hmc, k_arr, a_arr, NFW,
+                                                 normprof1=True, normprof2=True)
+
+        pk_hf = np.array([ccl.nonlin_matter_power(cosmo, k_arr, a)
+                          for a in a_arr])
         ratio = pk_hf / pk_hm
 
-        self.rk_func = interp2d(np.log10(karr), 1/(1+zarr), ratio,
+        self.rk_func = interp2d(np.log10(k_arr), a_arr, ratio,
                                 bounds_error=False, fill_value=1)
 
 
@@ -44,7 +57,7 @@ class HM_halofit(object):
         values at a given redshift.
 
         Args:
-            k (float or array): wavenumbers in units of Mpc^-1.
+            k (float or `numpy.ndarray`): wavenumbers in units of Mpc^-1.
             a (float): value of the scale factor.
         """
         return self.rk_func(np.log10(k), a)
@@ -60,16 +73,18 @@ class HaloModCorrection(object):
               information from the halo model correction, which is a fluke.
 
     Args:
-        cosmo (:obj:`ccl.Cosmology`): cosmology.
+        cosmo (`ccl.Cosmology`): cosmology.
         k_range (list): range of k to use (in Mpc^-1).
         nlk (int): number of samples in log(k) to use.
         z_range (list): range of redshifts to use.
         nz (int): number of samples in redshift to use.
+        **kwargs (dict): mass function and halo bias models
     """
     def __init__(self, cosmo,
                  k_range=[1E-1, 5], nlk=20,
-                 z_range=[0., 1.], nz=16):
-        hf = HM_halofit(cosmo).rk_interp
+                 z_range=[0., 1.], nz=16,
+                 **kwargs):
+        hf = HM_halofit(cosmo, **kwargs).rk_interp
         k_arr = np.geomspace(k_range[0], k_range[1], nlk)
         a_arr = 1/(1+np.linspace(z_range[0], z_range[1], nz))
 
@@ -92,8 +107,10 @@ class HaloModCorrection(object):
         Halo model correction as a function of wavenumber and scale factor.
 
         Args:
-        k (float or array): wavenumbers in units of Mpc^-1.
-        a (float or array): scale factor.
+        k (float or `numpy.ndarray`): wavenumbers in units of Mpc^-1.
+        a (float or `numpy.ndarray`): scale factor.
+        squeeze (bool): remove extra dimensions of no length.
+        **kwargs (dict): dictionary containing HM correction parameters.
 
         Returns:
             R (float ot array): halo model correction for given k
@@ -110,46 +127,3 @@ class HaloModCorrection(object):
 
         R = 1 + A*np.exp(-0.5*(np.log10(k/k0)/s)**2)
         return R.squeeze() if squeeze else R
-
-
-'''
-# # directly hard-coded; see comments at the end
-# BF_T08 = np.array([[0.51980569, 0.32320521], [0.52748408, 0.32734152],
-#                    [0.53517735, 0.33005779], [0.5435773 , 0.33215114],
-#                    [0.55340443, 0.33447197], [0.56455091, 0.33694753],
-#                    [0.57691618, 0.33949677], [0.59042371, 0.3420524 ],
-#                    [0.60500534, 0.34455816], [0.62061085, 0.34697507],
-#                    [0.63720212, 0.3492786 ], [0.65475726, 0.3514586 ],
-#                    [0.67325903, 0.35351153], [0.69270078, 0.35544179],
-#                    [0.71307605, 0.35725188], [0.73438947, 0.35895349]])
-
-# BF_T10 = np.array([[0.51767407, 0.32176539], [0.50331886, 0.30536391],
-#                    [0.49390514, 0.29210544], [0.48789828, 0.28137469],
-#                    [0.48471703, 0.2729755 ], [0.48400103, 0.26666773],
-#                    [0.4853959 , 0.262123  ], [0.48859967, 0.25902074],
-#                    [0.49336963, 0.25707979], [0.49951687, 0.25606673],
-#                    [0.5068935 , 0.25578902], [0.51538966, 0.25609536],
-#                    [0.52491646, 0.25686009], [0.53541016, 0.25798937],
-#                    [0.54681798, 0.25940262], [0.55910167, 0.26103996]])
-
-
-# def HM_gauss(mf):
-#     """
-#     Interpolates the best-fit gaussian approximation to the HM correction.
-
-#     Args:
-#         mf (str): cosmological mass function
-
-#     Returns:
-#         k0_func, s_func (func): interpolated functions for ``k0`` and ``s``
-#     """
-#     if mf == "tinker":
-#         k0_func = interp1d(hm_a, BF_T08[:, 0], bounds_error=False, fill_value=1)
-#         s_func = interp1d(hm_a, BF_T08[:, 1], bounds_error=False, fill_value=1e64)
-#     elif mf == "tinker10":
-#         k0_func = interp1d(hm_a, BF_T10[:, 0], bounds_error=False, fill_value=1)
-#         s_func = interp1d(hm_a, BF_T10[:, 1], bounds_error=False, fill_value=1e64)
-#     else:
-#         raise ValueError("Mass function not recognised in HM correction.")
-#     return k0_func, s_func
-'''
--- tests/hm_correction.py
+++ tests/hm_correction.py
@@ -1,12 +1,14 @@
 """
 Unit test for Gaussian approximation of the halo model correction.
-The approximation agrees with HaloFit within 4%.
+The approximation agrees with HaloFit within 5.5%.
 """
 
 import os, sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import numpy as np
 import pyccl as ccl
+from pyccl.halos.hmfunc import mass_function_from_name
+from pyccl.halos.hbias import halo_bias_from_name
 from model.hmcorr import HaloModCorrection
 from model.hmcorr import HM_halofit
 
@@ -18,13 +20,14 @@ cosmo = ccl.Cosmology(Omega_c=0.26066676,
                       Omega_b=0.048974682,
                       h=0.6766,
                       sigma8=0.8102,
-                      n_s=0.9665,
-                      mass_function="tinker")
+                      n_s=0.9665)
+kwargs = {"mass_function": mass_function_from_name("tinker08"),
+          "halo_bias": halo_bias_from_name("tinker10")}
 
-HM = HaloModCorrection(cosmo)
+HM = HaloModCorrection(cosmo, **kwargs)
 a_HMcorr = HM.af(a_arr)
 for i, a in enumerate(a_arr):
-    hm_old = HM_halofit(cosmo).rk_interp(k_arr, a)
-    kwargs = {"a_HMcorr": a_HMcorr[i]}
+    hm_old = HM_halofit(cosmo, **kwargs).rk_interp(k_arr, a)
+    kwargs["a_HMcorr"] = a_HMcorr[i]
     hm_new = HM.hm_correction(k_arr, a, **kwargs)
-    assert (np.fabs(1-hm_new/hm_old) < 0.04).all()
+    assert (np.fabs(1-hm_new/hm_old) < 0.053).all()
