0d97200f997e2e7afc92f3dc8306ded88eb4e510
nikfilippas@gmail.com <nikfilippas@gmail.com>
2020-07-06 14:32:56 +0300

    complete unit test for gxg and gxy

--- tests/cell.py
+++ tests/cell.py
@@ -1,6 +1,6 @@
 """
-Unit test for the CCL-based implementation of gxg.
-Comparison with old code agrees at a level better than 1%.
+Unit test for the CCL-based implementation of gxg and gxy.
+Comparison with old code agrees within 1% for gxg and 5% for gxy.
 """
 
 from analysis.params import ParamRun
@@ -10,44 +10,62 @@ from pyccl.halos.hbias import halo_bias_from_name
 from model.data import ProfTracer
 import numpy as np
 from model.power_spectrum import hm_ang_power_spectrum
-fname = "../../yxg/params_wnarrow.yml"
+fname = "../yxg/params_wnarrow.yml"
 p = ParamRun(fname)
-m = p.get("maps")[3]
-m["dndz"] = "../"+m["dndz"]
-
-p1 = p2 = ProfTracer(m)
 kwargs = p.get_models()["wisc3"]
 kwargs['lMmin'] = kwargs['Mmin']
 kwargs['lM0'] = kwargs['M0']
 kwargs['lM1'] = kwargs['M1']
 kwargs['sigmaLogM'] = kwargs['sigma_lnM']
-kwargs['width'] = 1
+kwargs['width'] = 1.
+kwargs["r_corr_gy"] = kwargs['r_corr']
 if p.get_massfunc() == "tinker":
     kwargs['mass_function'] = mass_function_from_name("tinker08")
     kwargs['halo_bias'] = halo_bias_from_name("tinker10")
 l = np.geomspace(6, 3000, 100)
-cl_new = hm_ang_power_spectrum(l, (p1, p2), **kwargs)
 
-cl_old = np.load("cl_test.npz")["cl"]
+# gxg
+for m in p.get("maps"):
+    if m["name"] == "wisc3":
+        break
+p1 = p2 = ProfTracer(m)
+
+cl_new = hm_ang_power_spectrum(l, (p1, p2), **kwargs)
+cl_old = np.load("tests/cl_test.npz")["clgg"]
 assert (np.fabs(1-cl_new/cl_old) < 0.001).all()
 
+# gxy
+for n in p.get("maps"):
+    if n["name"] == "y_milca":
+        break
+p2 = ProfTracer(n)
 
-# # Code to produce yxg cells
+cl_new = hm_ang_power_spectrum(l, (p1, p2), **kwargs)
+cl_old = np.load("tests/cl_test.npz")["clgy"]
+assert (np.fabs(1-cl_new/cl_old) < 0.05).all()
+
+
+# Code to produce yxg cells
 # import numpy as np
 # from analysis.params import ParamRun
 # from model.power_spectrum import hm_ang_power_spectrum
-# from model.profile2D import HOD
+# from model.profile2D import HOD, Arnaud
 # fname = "params_wnarrow.yml"
 # p = ParamRun(fname)
 # cosmo = p.get_cosmo()
+# y = Arnaud()
 # g = HOD(nz_file="data/dndz/WISC_bin3.txt")
 # l = np.geomspace(6, 3000, 100)
 # kwargs = p.get_models()["wisc3"]
 # kwargs['width'] = 1.
 # zrange = [0.001, 0.6]
 # zpoints = 256
-# cl = hm_ang_power_spectrum(cosmo, l, (g,g),
-#                             zrange=zrange,
-#                             zpoints=zpoints,
-#                             **kwargs)
-# np.savez("cl_test.npz", l=l, cl=cl)
+# clgg = hm_ang_power_spectrum(cosmo, l, (g,g),
+#                              zrange=zrange,
+#                              zpoints=zpoints,
+#                              **kwargs)
+# clgy = hm_ang_power_spectrum(cosmo, l, (g,y),
+#                              zrange=zrange,
+#                              zpoints=zpoints,
+#                              **kwargs)
+# np.savez("cl_test.npz", l=l, clgg=clgg, clgy=clgy)
