c3889a98731dc564565df9141798e070fb22413b
nikfilippas@gmail.com <nikfilippas@gmail.com>
2020-07-17 20:25:26 +0300

    implementation of new CCL in trispectrum

--- model/trispectrum.py
+++ model/trispectrum.py
@@ -4,91 +4,57 @@ from scipy.integrate import simps
 from .cosmo_utils import COSMO_ARGS
 
 
-def hm_1h_trispectrum(k, a, profiles,
-                      logMrange=(6, 17), mpoints=128,
-                      selection=None,
-                      kwargs_a=None, kwargs_b=None):
+def hm_1h_trispectrum(k, profiles, **kwargs):
     """Computes the halo model prediction for the 1-halo 3D
-    trispectrum of four quantities.
-
-    Args:
-        cosmo (:obj:`ccl.Cosmology`): cosmology.
-        k (array): array of wavenumbers in units of Mpc^-1
-        a (array): array of scale factor values
-        profiles (tuple): tuple of four profile objects (currently
-            only Arnaud and HOD are implemented) corresponding to
-            the four quantities being correlated.
-        logMrange (tuple): limits of integration in log10(M/Msun)
-        mpoints (int): number of mass samples
-        selection (function): selection function in (M,z) to include
-            in the calculation. Pass None if you don't want to select
-            a subset of the M-z plane.
-        kwargs_a (parameter dictionary): Parametrisation of the profiles
-            and cosmology for 1st cross-corr.
-        kwargs_b (parameter dictionary): Parametrisation of the profiles
-            and cosmology for 2nd cross-corr.
+    trispectrum of four profile quantities.
     """
-    pau, pav, pbu, pbv = profiles
-    cosmo = COSMO_ARGS(kwargs_a)
+    p1, p2, p3, p4 = profiles
+    cosmo = COSMO_ARGS(kwargs)
+    p1.update_parameters(cosmo, **kwargs)
+    p2.update_parameters(cosmo, **kwargs)
+    p3.update_parameters(cosmo, **kwargs)
+    p4.update_parameters(cosmo, **kwargs)
+    # Set up Halo Model Calculator
+    hmd = ccl.halos.MassDef(500, 'critical')
+    nM = kwargs["mass_function"](cosmo, mass_def=hmd)
+    bM = kwargs["halo_bias"](cosmo, mass_def=hmd)
+    hmc = ccl.halos.HMCalculator(cosmo, nM, bM, hmd)
 
-    aUnorm = pau.profnorm(a, squeeze=False, **kwargs_a)
-    aVnorm = pav.profnorm(a, squeeze=False, **kwargs_a)
-    bUnorm = pbu.profnorm(a, squeeze=False, **kwargs_b)
-    bVnorm = pbv.profnorm(a, squeeze=False, **kwargs_b)
-
-    logMmin, logMmax = logMrange
-    mpoints = int(mpoints)
-    M = np.logspace(logMmin, logMmax, mpoints)
-
-    Dm = pau.Delta/ccl.omega_x(cosmo, a, 'matter')
-    mfunc = np.array([ccl.massfunc(cosmo, M, aa, Dmm)
-                      for aa, Dmm in zip(a, Dm)]).T
-    if selection is not None:
-        select = np.array([selection(M,1./aa-1) for aa in a]).T
+    # covariance for p1, p2
+    if p1.type == p2.type == 'g':
+        p2pt_12 = ccl.halos.Profile2ptHOD()
     else:
-        select = 1
+        r_corr = kwargs.get('r_corr_%s%s' % (p1.type, p2.type))
+        if r_corr is None:
+            r_corr = kwargs.get('r_corr_%s%s' % (p2.type, p1.type))
+            if r_corr is None:
+                r_corr = 0
+                print('2pt covariance for %sx%s defaulting to 0' % (p1.type,
+                                                                    p2.type))
+        p2pt_12 = ccl.halos.Profile2ptR(r_corr=r_corr)
 
-    aU, aUU = pau.fourier_profiles(k, M, a, squeeze=False, **kwargs_a)
-    if pau.name == pav.name:
-        aUV = aUU
+    # covariance for p3, p4
+    if p3.type == p4.type == 'g':
+        p2pt_34 = ccl.halos.Profile2ptHOD()
     else:
-        aV, aVV = pav.fourier_profiles(k, M, a, squeeze=False, **kwargs_a)
-        if 'r_corr' in kwargs_a:
-            r = kwargs_a['r_corr']
-        else:
-            r = 0
-        aUV = np.sqrt(aUU*aVV)*(1+r)
+        r_corr = kwargs.get('r_corr_%s%s' % (p3.type, p4.type))
+        if r_corr is None:
+            r_corr = kwargs.get('r_corr_%s%s' % (p3.type, p4.type))
+            if r_corr is None:
+                r_corr = 0
+                print('2pt covariance for %sx%s defaulting to 0' % (p1.type,
+                                                                    p2.type))
+        p2pt_34 = ccl.halos.Profile2ptR(r_corr=r_corr)
 
-    bU, bUU = pbu.fourier_profiles(k, M, a, squeeze=False, **kwargs_b)
-    if pbu.name == pbv.name:
-        bUV = bUU
-    else:
-        bV, bVV = pbv.fourier_profiles(k, M, a, squeeze=False, **kwargs_b)
-        if 'r_corr' in kwargs_b:
-            r = kwargs_b['r_corr']
-        else:
-            r = 0
-        bUV = np.sqrt(bUU*bVV)*(1+r)
-
-    t1h = simps((select * mfunc)[:, :, None, None] *
-                aUV[:, :, :, None] *
-                bUV[:, :, None, :],
-                x=np.log10(M), axis=0)
-
-    rhoM = ccl.rho_x(cosmo, a, "matter", is_comoving=True)
-    dlM = (logMmax-logMmin) / (mpoints-1)
-    n0_1h = (rhoM-np.dot(M, mfunc)*dlM)/M[0]
-    t1h += (n0_1h[:, None, None]*aUV[0, :, :, None]*bUV[0, :, None, :])
-    t1h /= (aUnorm*aVnorm*bUnorm*bVnorm)[:, None, None]
-
-    return t1h
+    a_arr = np.linspace(0.2, 1, 128)
+    I04 = np.array([hmc.I_0_4(cosmo, k, a,
+                              p1.profile, p2pt_12, p2.profile,
+                              p3.profile, p2pt_34, p4.profile)
+                              for a in a_arr])
+    return I04
 
 
-def hm_ang_1h_covariance(fsky, l, profiles_a, profiles_b,
-                         zrange_a=(1e-6, 6), zpoints_a=32, zlog_a=True,
-                         zrange_b=(1e-6, 6), zpoints_b=32, zlog_b=True,
-                         logMrange=(6, 17), mpoints=128,
-                         selection=None, kwargs_a=None, kwargs_b=None):
+def hm_ang_1h_covariance(fsky, l, profiles, **kwargs):
     """Computes the 1-h trispectrum contribution to the covariance of the
     angular cross power spectra involving two pairs of quantities.
 
@@ -99,76 +65,25 @@ def hm_ang_1h_covariance(fsky, l, profiles_a, profiles_b,
     ----------
     fsky : float
         Sky fraction
-    l : array_like
-        The l-values (multipole number) of the cross power spectrum.
-    profiles_a : tuple of `profile2D._profile_` objects
-        The profiles for the first two quantities being correlated.
-    profiles_b : tuple of `profile2D._profile_` objects
-        The profiles for the second two quantities being correlated.
-    zrange_a : tuple
-        Minimum and maximum redshift probed for the first spectrum.
-    zpoints_a : int
-        Number or integration sampling points in redshift for the
-        first spectrum.
-    zlog_a : bool
-        Whether to use logarithmic spacing in redshifts for the first
-        spectrum.
-    zrange_b : tuple
-        Minimum and maximum redshift probed for the second spectrum.
-    zpoints : int
-        Number or integration sampling points in redshift for the
-        first spectrum.
-    zlog_b : bool
-        Whether to use logarithmic spacing in redshifts for the
-        first spectrum
-    logMrange : tuple
-        Logarithm (base-10) of the mass integration boundaries.
-    mpoints : int
-        Number or integration sampling points.
-    selection (function): selection function in (M,z) to include
-        in the calculation. Pass None if you don't want to select
-        a subset of the M-z plane.
-    kwargs_a : parameter dictionary
-        Parametrisation of the profiles and cosmology for 1st cross-corr.
-    kwargs_b : parameter dictionary
-        Parametrisation of the profiles and cosmology for 2nd cross-corr.
+    l (`numpy.array`): effective multipoles
+    profiles : tuple of `model.data.ProfTracer` objects
+        The profiles of the four quantities being correlated.
+    kwargs : parameter dictionary
+        Parametrisation of the profiles and cosmology.
     """
-    cosmo = COSMO_ARGS(kwargs_a)
-    zrange = np.array([min(np.amin(zrange_a), np.amin(zrange_b)),
-                       max(np.amax(zrange_a), np.amax(zrange_b))])
-    dz = min((zrange_a[1]-zrange_a[0])/zpoints_a,
-             (zrange_b[1]-zrange_b[0])/zpoints_b)
-    zpoints = int((zrange[1]-zrange[0])/dz)
-    zlog = zlog_a or zlog_b
+    cosmo = COSMO_ARGS(kwargs)
 
-    zmin, zmax = zrange
-    # Distance measures & out-of-loop optimisations
-    if zlog:
-        z = np.geomspace(zmin, zmax, zpoints)
-        jac = z
-        x = np.log(z)
-    else:
-        z = np.linspace(zmin, zmax, zpoints)
-        jac = 1
-        x = z
-    a = 1/(1+z)
+    a = np.linspace(0.2, 1-1e-6, 128)  # avoid zero division
     chi = ccl.comoving_radial_distance(cosmo, a)
 
-    H_inv = (2997.92458 * jac /
-             (ccl.h_over_h0(cosmo, a) * cosmo["h"]))  # c*z/H(z)
-    pau, pav = profiles_a
-    pbu, pbv = profiles_b
-    aWu = pau.kernel(a, **kwargs_a)
-    aWv = pav.kernel(a, **kwargs_b)
-    bWu = pbu.kernel(a, **kwargs_a)
-    bWv = pbv.kernel(a, **kwargs_b)
-    N = H_inv * aWu * aWv * bWu * bWv/chi**6
+    w1 = profiles[0].tracer.get_kernel(chi).squeeze()
+    w2 = profiles[1].tracer.get_kernel(chi).squeeze()
+    w3 = profiles[2].tracer.get_kernel(chi).squeeze()
+    w4 = profiles[3].tracer.get_kernel(chi).squeeze()
+    H_inv = (2997.92458*(1/a+1)/(ccl.h_over_h0(cosmo, a)*cosmo["h"]))  # c*z/H(z)
+    N = w1*w2*w3*w4*H_inv/(4*np.pi*fsky*chi**6)
 
-    k = (l+1/2) / chi[..., None]
-    t1h = hm_1h_trispectrum(k, a, (pau, pav, pbu, pbv),
-                            logMrange, mpoints, selection=selection,
-                            kwargs_a=kwargs_a, kwargs_b=kwargs_b)
-
-    tl = simps(N[:, None, None] * t1h, x, axis=0)
-
-    return tl / (4 * np.pi * fsky)
+    k = (l+1/2)/chi[:, np.newaxis]
+    t1h = hm_1h_trispectrum(k, profiles, **kwargs)
+    tl = simps(N[:, None, None] * t1h, np.log(1/a+1), axis=0)
+    return tl
