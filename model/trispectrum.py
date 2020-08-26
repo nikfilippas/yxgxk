7ab7dd19663200c2213802f9861722f348d5b621
David Alonso <dam.phys@gmail.com>
2020-08-26 10:43:48 +0100

    fixed trispectrum

--- model/trispectrum.py
+++ model/trispectrum.py
@@ -13,6 +13,9 @@ def hm_1h_trispectrum(cosmo, k, a, profiles, **kwargs):
     a_arr = np.atleast_1d(a)
 
     p1, p2, p3, p4 = profiles
+    for p in profiles:
+        p.update_parameters(cosmo, **kwargs)
+
     # Set up Halo Model Calculator
     hmd = ccl.halos.MassDef(500, 'critical')
     nM = kwargs["mass_function"](cosmo, mass_def=hmd)
@@ -24,20 +27,22 @@ def hm_1h_trispectrum(cosmo, k, a, profiles, **kwargs):
     p2pt_34 = get_2pt(p3, p4, **kwargs)
 
     I04 = []
-    for a in a_arr:
-        cov = hmc.I_0_4(cosmo, k, a,
+    for aa in a_arr:
+        cov = hmc.I_0_4(cosmo, k, aa,
                         p1.profile, p2pt_12, p2.profile,
                         p3.profile, p2pt_34, p4.profile)
         for p in profiles:
             if p.type != 'y':
-                cov *= hmc.profile_norm(cosmo, a, p.profile)
+                cov *= hmc.profile_norm(cosmo, aa, p.profile)
         I04.append(cov)
     I04 = np.asarray(I04)
 
     return I04
 
 
-def hm_ang_1h_covariance(fsky, l, cosmo, profiles, **kwargs):
+def hm_ang_1h_covariance(fsky, l, cosmo, profiles,
+                         zrange=(1e-6, 6), zpoints=32, zlog=True,
+                         **kwargs):
     """Computes the 1-h trispectrum contribution to the covariance of the
     angular cross power spectra involving two pairs of quantities.
 
@@ -55,7 +60,15 @@ def hm_ang_1h_covariance(fsky, l, cosmo, profiles, **kwargs):
     kwargs : parameter dictionary
         Parametrisation of the profiles and cosmology.
     """
-    z = np.linspace(1e-6, 4, 128)  # avoid zero division
+    zmin, zmax = zrange
+    if zlog:
+        z = np.geomspace(zmin, zmax, zpoints)
+        jac = z
+        x = np.log(z)
+    else:
+        z = np.linspace(zmin, zmax, zpoints)
+        jac = 1.
+        x = z
     a = 1/(1+z)
     chi = ccl.comoving_radial_distance(cosmo, a)
 
@@ -63,10 +76,34 @@ def hm_ang_1h_covariance(fsky, l, cosmo, profiles, **kwargs):
     w2 = profiles[1].tracer.get_kernel(chi).squeeze()
     w3 = profiles[2].tracer.get_kernel(chi).squeeze()
     w4 = profiles[3].tracer.get_kernel(chi).squeeze()
-    H_inv = (2997.92458/(ccl.h_over_h0(cosmo, a)*cosmo["h"]))  # c*z/H(z)
+    H_inv = (2997.92458 * jac/(ccl.h_over_h0(cosmo, a)*cosmo["h"]))  # c*z/H(z)
     N = w1*w2*w3*w4*H_inv/(4*np.pi*fsky*chi**6)
 
-    k = (l+1/2)/chi[:, np.newaxis]
-    t1h = hm_1h_trispectrum(cosmo, k, profiles, **kwargs)
-    tl = simps(N[:, None, None] * t1h, z, axis=0)
+    COSMO_CHECK(cosmo, **kwargs)
+
+    p1, p2, p3, p4 = profiles
+    for p in profiles:
+        p.update_parameters(cosmo, **kwargs)
+
+    # Set up Halo Model Calculator
+    hmd = ccl.halos.MassDef(500, 'critical')
+    nM = kwargs["mass_function"](cosmo, mass_def=hmd)
+    bM = kwargs["halo_bias"](cosmo, mass_def=hmd)
+    hmc = ccl.halos.HMCalculator(cosmo, nM, bM, hmd)
+
+    # set up covariance
+    p2pt_12 = get_2pt(p1, p2, **kwargs)
+    p2pt_34 = get_2pt(p3, p4, **kwargs)
+
+    t1h = np.zeros([len(a), len(l), len(l)])
+    for ii, (aa, cchi) in enumerate(zip(a, chi)):
+        k = (l+1/2)/cchi
+        cov = hmc.I_0_4(cosmo, k, aa,
+                        p1.profile, p2pt_12, p2.profile,
+                        p3.profile, p2pt_34, p4.profile)
+        for p in profiles:
+            if p.type != 'y':
+                cov *= hmc.profile_norm(cosmo, aa, p.profile)
+        t1h[ii, :, :] = cov
+    tl = simps(N[:, None, None] * t1h, x, axis=0)
     return tl
