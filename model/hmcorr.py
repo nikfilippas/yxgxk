264a83885bf58f96b31ab3f01c224bef25784165
nikfilippas <nikfilippas@gmail.com>
2020-10-08 16:52:06 +0300

    improved calcs and compatibility

--- model/hmcorr.py
+++ model/hmcorr.py
@@ -8,6 +8,7 @@ from scipy.interpolate import interp1d
 from scipy.interpolate import interp2d
 from scipy.optimize import curve_fit
 import warnings
+from .utils import get_hmcalc
 
 
 class HM_halofit(object):
@@ -26,21 +27,26 @@ class HM_halofit(object):
 
     .. note: original HaloFit used `rho_200m`
     .. note: ``interp2d`` flips secondary axis
+    .. note: non-linear prediction accurate up to `k~5`
     """
     def __init__(self, cosmo,
-                  k_range=[1E-1, 5], nlk=20,
-                  z_range=[0., 1.], nz=16,
+                  k_range=[1e-3, 5], nlk=128,
+                  z_range=[0., 1.], nz=32,
                   Delta=200, rho_type='matter',
                   **kwargs):
+
         k_arr = np.geomspace(k_range[0], k_range[1], nlk)
         a_arr = 1/(1+np.linspace(z_range[0], z_range[1], nz))
 
         hmd = ccl.halos.MassDef(Delta, rho_type)
-        nM = kwargs["mass_function"](cosmo, mass_def=hmd)
-        bM = kwargs["halo_bias"](cosmo, mass_def=hmd)
-        hmc = ccl.halos.HMCalculator(cosmo, nM, bM, hmd)
-        cM = ccl.halos.ConcentrationDuffy08(hmd)
+        if (Delta, rho_type) == (200, "matter"):
+            cM = ccl.halos.ConcentrationDuffy08(hmd)
+        elif (Delta, rho_type) == (500, "critical"):
+            cM = ccl.halos.halos_extra.ConcentrationDuffy08M500c(hmd)
+        else:
+            raise ValueError("c(M) relation for Delta=(%d %s) not implemented." % (Delta, rho_type))
         NFW = ccl.halos.profiles.HaloProfileNFW(cM)
+        hmc = get_hmcalc(cosmo, Delta, rho_type, **kwargs)
         pk_hm = ccl.halos.halomod_power_spectrum(cosmo, hmc, k_arr, a_arr, NFW,
                                                  normprof1=True, normprof2=True)
 
@@ -52,7 +58,7 @@ class HM_halofit(object):
                                 bounds_error=False, fill_value=1)
 
 
-    def rk_interp(self, k, a):
+    def rk_interp(self, k, a, **kwargs):
         """
         Returns the halo model correction for an array of k
         values at a given redshift.
@@ -60,12 +66,13 @@ class HM_halofit(object):
         Args:
             k (float or `numpy.ndarray`): wavenumbers in units of Mpc^-1.
             a (float): value of the scale factor.
+            kwargs (dict): empty dictionary
         """
         return self.rk_func(np.log10(k), a)
 
 
 
-class HaloModCorrection(object):
+class HM_Gauss(object):
     """
     Approximates the halo model correction as a gaussian with mean ``mu``
     and standard deviation ``sigma``.
@@ -79,10 +86,12 @@ class HaloModCorrection(object):
         z_range (list): range of redshifts to use.
         nz (int): number of samples in redshift to use.
         kwargs (dict): mass function and halo bias models
+
+        .. note: Same `kmax` as HALOFIT since we calibrate against it
     """
     def __init__(self, cosmo,
-                 k_range=[1E-1, 5], nlk=20,
-                 z_range=[0., 4.], nz=64,
+                 k_range=[1e-3, 5], nlk=128,
+                 z_range=[0., 1.], nz=32,
                  **kwargs):
         hf = HM_halofit(cosmo, **kwargs).rk_interp
         k_arr = np.geomspace(k_range[0], k_range[1], nlk)
@@ -91,7 +100,7 @@ class HaloModCorrection(object):
         gauss = lambda k, A, k0, s: 1 + A*np.exp(-0.5*(np.log10(k/k0)/s)**2)
 
         POPT = [[] for i in range(a_arr.size)]
-        # catch covariance errors due to the fill_value step
+        # catch covariance errors due to the `fill_value` step
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
             for i, a in enumerate(a_arr):
@@ -101,7 +110,7 @@ class HaloModCorrection(object):
         BF = np.vstack(POPT)
 
         self.af = interp1d(a_arr, BF[:, 0], bounds_error=False, fill_value="extrapolate")
-        self.k0f = interp1d(a_arr, BF[:, 1], bounds_error=False, fill_value=1)
+        self.k0f = interp1d(a_arr, BF[:, 1], bounds_error=False, fill_value=1.)
         self.sf = interp1d(a_arr, BF[:, 2], bounds_error=False, fill_value=1e64)
 
 
