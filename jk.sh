cda5f2834f46e1493f8e226e62549ca7a40d464b
nikfilippas <nikfilippas@gmail.com>
2020-10-09 17:48:59 +0300

    updated npatches

--- jk.sh
+++ jk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for jk in {297..388}
+for jk in {1..456}
 do
-    addqueue -s -c "jk ${jk}" -n 1x8 -q "cmb" -m 4 '/mnt/zfsusers/nikfilippas/anaconda3/bin/python' pipeline.py params_lensing.yml --jk-id ${jk}
+    addqueue -s -c "jk ${jk}" -n 1x12 -q "cmb" -m 4 '/mnt/zfsusers/nikfilippas/anaconda3/bin/python' pipeline.py params_lensing.yml --jk-id ${jk}
 done
