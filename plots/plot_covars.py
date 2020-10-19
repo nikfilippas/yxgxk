8b5d9a40c65098e8298785a931bc6e359881c83b
nikfilippas <nikfilippas@gmail.com>
2020-10-19 13:38:56 +0300

    added k option

--- plots/plot_covars.py
+++ plots/plot_covars.py
@@ -11,8 +11,9 @@ from matplotlib import cm
 import pyccl as ccl
 from matplotlib import rc
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
-rc('text', usetex=True)
+rc('text', usetex=False)
 
+pref, cov_type = 'd', '1h4pt'
 cosmo=ccl.Cosmology(Omega_c=0.26066676,
                     Omega_b=0.048974682,
                     h=0.6766,
@@ -37,39 +38,59 @@ def plot_covar(predir,sample,bn,sample_label,kmax=1.,lmin=0.):
     l = np.load("output_default/cls_"+gsample+'_'+gsample+".npz")['ls']
     msk_gg=(l<=lmax) & (l>=lmin)
     msk_gy=(l<=lmax)
+    msk_gk=(l<=lmax)
     ngg=np.sum(msk_gg)
     ngy=np.sum(msk_gy)
-    nt=ngg+ngy
-    cv_gg_gg=np.load(predir+'/cov_comb_m_'+
+    ngk=np.sum(msk_gk)
+    nt=ngg+ngy+ngk
+    cv_gg_gg=np.load(predir+'/'+pref+'cov_'+cov_type+'_'+
                      gsample+'_'+gsample+'_'+
                      gsample+'_'+gsample+'.npz')['cov'][msk_gg,:][:,msk_gg]
-    cv_gg_gy=np.load(predir+'/cov_comb_m_'+
+    cv_gg_gy=np.load(predir+'/'+pref+'cov_'+cov_type+'_'+
                      gsample+'_'+gsample+'_'+
                      gsample+'_y_milca.npz')['cov'][msk_gg,:][:,msk_gy]
-    cv_gy_gy=np.load(predir+'/cov_comb_m_'+
+    cv_gg_gk=np.load(predir+'/'+pref+'cov_'+cov_type+'_'+
+                     gsample+'_'+gsample+'_'+
+                     gsample+'_lens.npz')['cov'][msk_gg,:][:,msk_gk]
+    cv_gy_gy=np.load(predir+'/'+pref+'cov_'+cov_type+'_'+
                      gsample+'_y_milca_'+
                      gsample+'_y_milca.npz')['cov'][msk_gy,:][:,msk_gy]
+    cv_gy_gk=np.load(predir+'/'+pref+'cov_'+cov_type+'_'+
+                     gsample+'_y_milca_'+
+                     gsample+'_lens.npz')['cov'][msk_gy,:][:,msk_gk]
+    cv_gk_gk=np.load(predir+'/'+pref+'cov_'+cov_type+'_'+
+                     gsample+'_lens_'+
+                     gsample+'_lens.npz')['cov'][msk_gk,:][:,msk_gk]
     cov=np.zeros([nt,nt])
-    cov[:ngg,:ngg]=cv_gg_gg
-    cov[:ngg,ngg:]=cv_gg_gy
-    cov[ngg:,:ngg]=cv_gg_gy.T
-    cov[ngg:,ngg:]=cv_gy_gy
+    cov[:ngg, :ngg] = cv_gg_gg
+    cov[:ngg, ngg:ngg+ngy] = cv_gg_gy
+    cov[:ngg, ngg+ngy:] = cv_gg_gk
+    cov[ngg:ngg+ngy, :ngg] = cv_gg_gy.T
+    cov[ngg:ngg+ngy, ngg:ngg+ngy] = cv_gy_gy
+    cov[ngg:ngg+ngy, ngg+ngy:] = cv_gy_gk
+    cov[ngg+ngy:, :ngg] = cv_gg_gk.T
+    cov[ngg+ngy:, ngg:ngg+ngy] = cv_gy_gk.T
+    cov[ngg+ngy:, ngg+ngy:] = cv_gk_gk
 
     plt.figure()
     ax=plt.gca()
     im=ax.imshow(get_corr(cov),interpolation='nearest',
                  vmin=0,vmax=1,cmap=cm.gray)
     ax.tick_params(length=0)
-    ax.set_xticks([ngg/2,ngg+ngy/2])
-    ax.set_xticklabels(['$g\\,\\times\\,g$','$g\\,\\times\\,y$'])
-    ax.set_yticks([0.4*ngg,ngg+0.4*ngy])
-    ax.set_yticklabels(['$g\\,\\times\\,g$','$g\\,\\times\\,y$'],
-                       rotation=90)
+    ax.set_xticks([ngg/2, ngg+ngy/2, ngg+ngy+ngk/2])
+    ax.set_xticklabels(['$g\\,\\times\\,g$',
+                        '$g\\,\\times\\,y$',
+                        '$g\\,\\times\\,\\kappa$'])
+    ax.set_yticks([0.4*ngg, ngg+0.4*ngy, ngg+ngy+0.4*ngk])
+    ax.set_yticklabels(['$g\\,\\times\\,g$',
+                        '$g\\,\\times\\,y$',
+                        '$g\\,\\times\\,\\kappa$'], rotation=90)
     ax.text(0.03,0.04,sample_label,transform=ax.transAxes,
             bbox=dict(facecolor='white', alpha=1),fontsize=14)
     ax.tick_params(labelsize="x-large")
-    plt.savefig('notes/paper_yxg/cov_'+gsample+'.pdf',
-                bbox_inches='tight')
+    # plt.savefig("/home/nick/Desktop/cov_"+gsample+".pdf", bbox_inches="tight")
+    # plt.savefig('notes/paper_yxg/cov_'+gsample+'.pdf',
+    #             bbox_inches='tight')
 
 plot_covar("output_default","2mpz",1,'2MPZ',lmin=0.)
 for b in range(5):
