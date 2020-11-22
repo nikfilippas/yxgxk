f7cb8950d0ab5c7aa41ce22fd01de2e5f64bd436
Nick Koukoufilippas <nikfilippas@gmail.com>
2020-11-23 01:31:33 +0200

    cmb q

--- jk.sh
+++ jk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for jk in {0..455}
 do
-    addqueue -s -c "jk ${jk}" -n 1x12 -q "berg" -m 4 '/mnt/zfsusers/nikfilippas/anaconda3/bin/python' pipeline.py params_lensing.yml --jk-id ${jk}
+    addqueue -s -c "jk ${jk}" -n 1x12 -q "cmb" -m 4 '/mnt/zfsusers/nikfilippas/anaconda3/bin/python' pipeline.py params_lensing.yml --jk-id ${jk}
 done
