7eb0eaf2ada25a10c920580882642e7843d9026f
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-05-25 16:47:06 +0300

    re-ordered for better debugging

--- test_cobaya.py
+++ test_cobaya.py
@@ -3,6 +3,7 @@ from cobaya.model import get_model
 from cobaya.run import run
 import yaml
 
+# fname_params = "par_2mpz.yaml"
 fname_params = sys.argv[1]
 
 with open(fname_params, "r") as fin:
@@ -16,9 +17,9 @@ for p in info['params']:
 
 os.system('mkdir -p ' + info['output'])
 
-print(p0)
 model = get_model(info)
 loglikes, derived = model.loglikes(p0)
+print(p0)
 print("chisq:", -2 * loglikes)
 exit(1)
 updated_info, sampler = run(info)
