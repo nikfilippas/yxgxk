8030bfa7c2592d34feff9ec0e85a8c110cfcb4b8
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-08-12 19:53:23 +0300

    removed many unnecessary funcs

--- model/cosmo_utils.py
+++ model/cosmo_utils.py
@@ -2,42 +2,11 @@
 Names of all cosmological parameters accepted by CCL.
 https://ccl.readthedocs.io/en/latest/source/notation_and_other_cosmological_conventions.html
 """
+from inspect import signature
 import pyccl as ccl
 
 
-COSMO_KEYS = ['Omega_c',                    # background parameters
-              'Omega_b',
-              'h',
-              'Omega_k',
-              'Omega_g',
-              'w0',
-              'wa',
-
-              'sigma8',                     # power spectrum normalisation
-              'A_s',
-              'n_s',
-
-              'Neff',                       # relativistic species
-              'm_nu',
-              'T_CMB',
-
-              'mu_0',                       # modified gravity parameters
-              'sigma_0',
-              'z_mg',
-              'df_mg',
-
-              'bcm_log10Mc',                # baryonic correction model
-              'bcm_etab',
-              'bcm_ks',
-
-              'transfer_function',          # model specifications
-              'matter_power_spectrum',
-              'baryons_power_spectrum',
-              # 'mass_function',            # ! deprecated in CCL
-              'halo_concentration',
-              'm_nu_type',
-              'emulator_neutrinos']
-
+COSMO_KEYS = list(signature(ccl.Cosmology).parameters.keys())
 
 def COSMO_DEFAULT():
     """Returns ``pyccl.Cosmology`` object for default Cosmology."""
@@ -61,46 +30,46 @@ def COSMO_DEFAULT():
 #         return ccl.Cosmology(**cosmoargs)
 
 
-def COSMO_ARGS(kwargs, transfer=None):
-    """ Uses the emulator to produce cosmology. """
-    C = transfer if transfer is not None else ccl
-    cosmoargs = {k: kwargs[k] for k in kwargs if k in COSMO_KEYS}
-    if not cosmoargs:
-        cosmo = COSMO_DEFAULT()
-        if not cosmo.has_growth:
-            cosmo.compute_growth()
-        return cosmo
-    else:
-        cosmo = C.Cosmology(**cosmoargs)
-        if not cosmo.has_growth:
-            cosmo.compute_growth()
-        return cosmo
+# def COSMO_ARGS(kwargs, transfer=None):
+#     """ Uses the emulator to produce cosmology. """
+#     C = transfer if transfer is not None else ccl
+#     cosmoargs = {k: kwargs[k] for k in kwargs if k in COSMO_KEYS}
+#     if not cosmoargs:
+#         cosmo = COSMO_DEFAULT()
+#         if not cosmo.has_growth:
+#             cosmo.compute_growth()
+#         return cosmo
+#     else:
+#         cosmo = C.Cosmology(**cosmoargs)
+#         if not cosmo.has_growth:
+#             cosmo.compute_growth()
+#         return cosmo
 
 
-def COSMO_CHECK(cosmo, **kwargs):
-    """
-    Verifies that the cosmology object passed is in line
-    with the model parameters.
+# def COSMO_CHECK(cosmo, **kwargs):
+#     """
+#     Verifies that the cosmology object passed is in line
+#     with the model parameters.
 
-    Parameters
-    ----------
-    cosmo : (~pyccl.core.Cosmology)
-        A cosmology object.
-    kwargs : dict
-        The dictionary including cosmological parameters.
+#     Parameters
+#     ----------
+#     cosmo : (~pyccl.core.Cosmology)
+#         A cosmology object.
+#     kwargs : dict
+#         The dictionary including cosmological parameters.
 
-    Returns
-    -------
-    None.
-    """
-    for k in kwargs:
-        if k in COSMO_KEYS:
-            assert kwargs[k] == cosmo[k], 'Mismatch in %s passed.' % k
+#     Returns
+#     -------
+#     None.
+#     """
+#     for k in kwargs:
+#         if k in COSMO_KEYS:
+#             assert kwargs[k] == cosmo[k], 'Mismatch in %s passed.' % k
 
 
-def COSMO_VARY(p):
-    """
-    Checks if Cosmology varies in the current analysis.
-    """
-    vary = [par["vary"] for par in p.get("params") if par["name"] in COSMO_KEYS]
-    return any(vary)
+# def COSMO_VARY(p):
+#     """
+#     Checks if Cosmology varies in the current analysis.
+#     """
+#     vary = [par["vary"] for par in p.get("params") if par["name"] in COSMO_KEYS]
+#     return any(vary)
