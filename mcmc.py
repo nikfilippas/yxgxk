33aa4c73b01217f046d2ef4e4a2d2923d5f62e30
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-08-12 19:52:55 +0300

    polished for immediate communication with analysis/params

--- mcmc.py
+++ mcmc.py
@@ -1,15 +1,11 @@
 from argparse import ArgumentParser
 import numpy as np
 from analysis.params import ParamRun
-from likelihood.yaml_handler import update_params
 from likelihood.like import Likelihood
 from likelihood.sampler import Sampler
 from model.data import DataManager
 from model.theory import get_theory
-from model.utils import get_hmcalc
-from model.cosmo_utils import COSMO_VARY, COSMO_ARGS
 from model.hmcorr import HM_halofit
-from likelihood.ccl_baccoemu import ccl_baccoemu
 import pyccl as ccl
 
 
@@ -23,14 +19,7 @@ args = parser.parse_args()
 fname_params = args.fname_params
 
 p = ParamRun(fname_params)
-cosmo_vary = COSMO_VARY(p)  # vary cosmology in this analysis?
-if p.get("mcmc")["transfer_function"] == "baccoemu":
-    cc = ccl_baccoemu()  # load emulator
-else:
-    cc = None
-
-kw = p.get_cosmo_pars()
-hm_correction = HM_halofit(ccl.CosmologyVanillaLCDM(), **kw).rk_interp
+hm_correction = HM_halofit(ccl.CosmologyVanillaLCDM(), p.get_hmc()).rk_interp
 
 # Jackknives
 jk_region = args.jk_id
@@ -52,55 +41,33 @@ for v in p.get('data_vectors'):
     # Construct data vector and covariance
     d = DataManager(p, v, jk_region=jk_region)
 
-    # Extract cosmology from model of this z-bin
-    for m in p.get("maps"):
-        if m["name"] != v["name"]:
-            continue
-        else:
-            model = m["model"]
-            cpars = {"Omega_c": model["Omega_c"],
-                     "Omega_b": model["Omega_b"],
-                     "h": model["h"],
-                     "sigma8": model["sigma8"],
-                     "n_s": model["n_s"]}
-            break
-
     # Theory predictor wrapper
-    if not cosmo_vary:
+    hmc = p.get_hmc()  # cosmology-independent
+    if not p.cosmo_vary:
         cosmo = p.get_cosmo()
-        hmc = get_hmcalc(mass_function=p.get_massfunc(),
-                         halo_bias=p.get_halobias())
-
         def th(kwargs):
             """Theory for fixed cosmology."""
-            cosmo_fid = cosmo
-            hmc_fid = hmc
-            return get_theory(p, d, cosmo_fid, hmc_fid,
+            return get_theory(p, d, cosmo, hmc,
                               hm_correction=hm_correction,
                               **kwargs)
     else:
-        temp = {"mass_function": p.get_massfunc(),
-                "halo_bias": p.get_halobias()}
-
         def th(kwargs):
             """Theory for free cosmology."""
-            kwargs = {**temp, **kwargs}
-            cosmo_fid = COSMO_ARGS(kwargs, transfer=cc)
-            hmc_fid = get_hmcalc(**kwargs)
-            return get_theory(p, d, cosmo_fid, hmc_fid,
+            cosmo_use = p.get_cosmo(pars=kwargs)
+            return get_theory(p, d, cosmo_use, hmc,
                               hm_correction=hm_correction,
                               **kwargs)
 
     # Set up likelihood
     lik = Likelihood(p.get('params'), d.data_vector, d.covar, th,
                      template=d.templates, debug=p.get('mcmc')['debug'])
-
     # Set up sampler
     p0 = extract_map_p0(p, v, lik.p_free_names)  # p0 for particular map
-    # Benchmarks
+
     # print(dict(zip(lik.p_free_names, p0)))
     # print("chisq:", lik.chi2(p0))
     # exit(1)
+
     sam = Sampler(lik.lnprob, p0, lik.p_free_names,
                   p.get_sampler_prefix(v['name']),
                   p.get('mcmc'))
@@ -117,10 +84,6 @@ for v in p.get('data_vectors'):
         print("  " + n + " : %.3lE +- %.3lE" % (val, s))
     print(" chi^2 = %lf" % (lik.chi2(sam.p0)))
     print(" n_data = %d" % (len(d.data_vector)))
-
-    # Update yaml file with best-fit parameters
-    # update_params(fname_params, v["name"], sam.parnames, sam.p0)
-    # print("Updated yaml file with best-fit parameters.")
 
     if sam.nsteps > 0:
         # Monte-carlo
