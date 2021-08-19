e1b14e61acb2d10a951d9ee8e57bc5c0a1f314da
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-08-19 13:54:51 +0300

    run all zbins together

--- run_mcmc.sh
+++ run_mcmc.sh
@@ -3,5 +3,5 @@ declare -a arr=("2mpz" "wisc1" "wisc2" "wisc3" "wisc4" "wisc5")
 
 for i in "${arr[@]}"
 do
-  addqueue -q cmb -n 48 -m 3 /mnt/zfsusers/nikfilippas/anaconda3/bin/python mcmc.py $1 "$i"
+  addqueue -q cmb -n 48 -m 3 /mnt/zfsusers/nikfilippas/anaconda3/bin/python mcmc.py $1 -dv "$i"
 done
